7f75276d0a2952cdfd91a80e60043ba121a066bb
anasraza07 <ar470684@gmail.com>
2024-07-23 06:35:31 +0500

    arrays lecture noted and understood plus one coding practice file added

---
+++ Chai aur Code/index.js
@@ -0,0 +1,49 @@
+const arr = [1, 2, 3, 4];
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = 'Hello world!';
+for (const greet of greetings) {
+    if (greet === ' ' || greet === '!') continue;
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps
+const map = new Map();
+map.set('In', 'India');
+map.set('PK', 'Pakistan');
+map.set('In', 'India');
+// console.log(map);
+
+for (const key of map) {
+    // console.log(key);
+}
+
+for (const [key, values] of map) {
+    // console.log(key, ':-', values);
+}
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ['js', 'cpp', 'swift'];
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+const myNums = [1, 2, 3];
+
+const myTotal = myNums.reduce((acc, currVal) => {
+    console.log(`acc is ${acc} and current value is ${currVal}`)
+    return acc + currVal
+}, 0);
+
+console.log(myTotal);
